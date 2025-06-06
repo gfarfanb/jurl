feecc00b604623226beab0c544a4dff3a8682743
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-06-06 00:22:06 +0000

    Date time for constants

--- src/test/java/com/legadi/cli/jurl/generators/DateTimeGeneratorTest.java
+++ src/test/java/com/legadi/cli/jurl/generators/DateTimeGeneratorTest.java
@@ -32,6 +32,25 @@ public class DateTimeGeneratorTest extends GeneratorAbstractTest {
     }
 
     @Test
+    public void dateTimeFormatFromConstants() {
+        Assertions.assertNotNull(generate("BASIC_ISO_DATE"));
+        Assertions.assertNotNull(generate("ISO_LOCAL_DATE"));
+        Assertions.assertNotNull(generate("ISO_OFFSET_DATE"));
+        Assertions.assertNotNull(generate("ISO_DATE"));
+        Assertions.assertNotNull(generate("ISO_LOCAL_TIME"));
+        Assertions.assertNotNull(generate("ISO_OFFSET_TIME"));
+        Assertions.assertNotNull(generate("ISO_TIME"));
+        Assertions.assertNotNull(generate("ISO_LOCAL_DATE_TIME"));
+        Assertions.assertNotNull(generate("ISO_OFFSET_DATE_TIME"));
+        Assertions.assertNotNull(generate("ISO_ZONED_DATE_TIME"));
+        Assertions.assertNotNull(generate("ISO_DATE_TIME"));
+        Assertions.assertNotNull(generate("ISO_ORDINAL_DATE"));
+        Assertions.assertNotNull(generate("ISO_WEEK_DATE"));
+        Assertions.assertNotNull(generate("ISO_INSTANT"));
+        Assertions.assertNotNull(generate("RFC_1123_DATE_TIME"));
+    }
+
+    @Test
     public void dateTimeWrongArg() {
         Assertions.assertThrows(CommandException.class,
             () -> generate("year-month-day"));
