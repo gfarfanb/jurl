44444c2604cfd5ecb7acde96887246d8a5c68e54
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-08-08 18:14:37 +0000

    Base64 operation

--- src/main/java/com/legadi/cli/jurl/modifiers/Base64ValueModifier.java
+++ src/main/java/com/legadi/cli/jurl/modifiers/Base64ValueModifier.java
@@ -3,6 +3,8 @@ package com.legadi.cli.jurl.modifiers;
 import java.util.Base64;
 import java.util.function.Function;
 
+import com.legadi.cli.jurl.exception.CommandException;
+
 public class Base64ValueModifier implements ValueModifier {
 
     @Override
@@ -12,13 +14,21 @@ public class Base64ValueModifier implements ValueModifier {
 
     @Override
     public String[] getArgs() {
-        return new String[0];
+        return new String[] { "operation" };
     }
 
     @Override
     public String apply(Function<String, String> propertyResolver, String[] args, String value) throws Exception {
         String input = propertyResolver.apply(value);
-        byte[] encoded = Base64.getEncoder().encode(input.getBytes());
-        return new String(encoded);
+        String operation = args[0].toLowerCase();
+
+        switch(operation) {
+            case "encode":
+                return new String(Base64.getEncoder().encode(input.getBytes()));
+            case "decode":
+                return new String(Base64.getDecoder().decode(input.getBytes()));
+            default:
+                throw new CommandException("Invalid Base 64 operation: " + operation);
+        }
     }
 }
--- src/test/java/com/legadi/cli/jurl/modifiers/Base64ValueModifierTest.java
+++ src/test/java/com/legadi/cli/jurl/modifiers/Base64ValueModifierTest.java
@@ -5,6 +5,8 @@ import java.util.Base64;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.legadi.cli.jurl.exception.CommandException;
+
 public class Base64ValueModifierTest extends ValueModifierAbstractTest<Base64ValueModifier> {
 
     public Base64ValueModifierTest() {
@@ -13,17 +15,36 @@ public class Base64ValueModifierTest extends ValueModifierAbstractTest<Base64Val
 
     @Override
     public String[] sampleValidArgs() {
-        return new String[0];
+        return new String[] { "encode" };
     }
 
     @Test
     public void encode() {
-        String[] args = {};
+        String[] args = { "encode" };
         String result = Assertions.assertDoesNotThrow(
             () -> apply(args, "{\"name\": \"test\"}"));
 
         String encoded = new String(Base64.getEncoder().encode("{\"name\": \"test\"}".getBytes()));
 
         Assertions.assertEquals(encoded, result);
+    }
+
+    @Test
+    public void decode() {
+        String[] args = { "decode" };
+        String result = Assertions.assertDoesNotThrow(
+            () -> apply(args, "eyJuYW1lIjogInRlc3QifQ=="));
+
+        String decoded = new String(Base64.getDecoder().decode("eyJuYW1lIjogInRlc3QifQ==".getBytes()));
+
+        Assertions.assertEquals(decoded, result);
+    }
+
+    @Test
+    public void invalid() {
+        String[] args = { "invalid" };
+        
+        Assertions.assertThrows(CommandException.class,
+            () -> apply(args, "eyJuYW1lIjogInRlc3QifQ=="));
     }
 }
